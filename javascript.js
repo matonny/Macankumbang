ddcbb8465125b0ffcf64ca91efa650d0464055f2
matonny <onny.mat@gmail.com>
2017-05-02 19:49:15 +0200

    finished
    yay

---
+++ javascript.js
@@ -0,0 +1,24 @@
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+function initMap() {
+  var uluru = {lat:50.210694 , lng: -5.483915};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
